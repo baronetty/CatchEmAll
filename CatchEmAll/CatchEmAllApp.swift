1ae1f6f941fa959db4159dc60b6d0f542cdec0f9
Leo Barnett <leo.e.barnett@icloud.com>
2024-04-02 10:35:02 +0200

    Initial Commit

---
+++ CatchEmAll/CatchEmAllApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchEmAllApp.swift
+//  CatchEmAll
+//
+//  Created by Leo  on 02.04.24.
+//
+
+import SwiftUI
+
+@main
+struct CatchEmAllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
